e581d79592e3b11ba349b4aefe039810db5bc94d
shmansuro <thesameshad@gmail.com>
2026-02-09 17:54:17 +0600

    feat: added handlers

--- app/main.go
+++ app/main.go
@@ -4,15 +4,18 @@ import (
 	"flag"
 	"log"
 	"triple-s/internal/cmd"
+	"triple-s/internal/server"
 )
 
 func main() {
 	port := flag.Int("port", 8080, "")
-	dir := flag.String("dir", "/data", "")
+	dir := flag.String("dir", "data", "")
 	help := flag.Bool("help", false, "")
 	flag.Parse()
 	if err := cmd.ArgsRoute(*port, *dir, *help); err != nil {
 		log.Fatalf("Братья, у нас проблемы тут с %v", err)
 		return
 	}
+
+	log.Fatal(server.StartServer(*port))
 }
--- internal/handlers/root.go
+++ internal/handlers/root.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+func Root(w http.ResponseWriter, r *http.Request) {
+	apiURL := r.URL.Path
+	parts := strings.Split(strings.Trim(apiURL, "/"), "/")
+	if len(parts) > 2 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Bag Request"))
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+
+	case http.MethodPut:
+	case http.MethodDelete:
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Server is running"))
+}
